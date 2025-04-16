25fbb51686747c1c77019df1834b13ba5b624a28
DhouhaOun <Dhouha.ouni93@gmail.com>
2025-04-16 11:27:25 +0200

    add config for customer-service

--- customer-service/src/main/java/micro/project/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/micro/project/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,16 @@
 package micro.project.customerservice;
 
+import micro.project.customerservice.config.CustomerConfigParams;
 import micro.project.customerservice.entities.Customer;
 import micro.project.customerservice.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
